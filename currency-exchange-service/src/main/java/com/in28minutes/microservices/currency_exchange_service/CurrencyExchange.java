224ac08ecfada1b1d5dccbccde28679f517b8929
viru0909-dev <gadekarvirendra4@gmail.com>
2025-05-05 11:34:11 +0530

    Fix data.sql IDs, update repository, controller, and add conversion service

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/CurrencyExchange.java
@@ -1,11 +1,23 @@
 package com.in28minutes.microservices.currency_exchange_service;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 
+@Entity
 public class CurrencyExchange {
+
+    @Id
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
+
     private BigDecimal  conversionMultiple;
 
     private String environment;
---
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/CurrencyExchangeRepository.java
@@ -0,0 +1,9 @@
+package com.in28minutes.microservices.currency_exchange_service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange,Long> {
+
+    CurrencyExchange findByFromAndTo(String from, String to);
+
+}
